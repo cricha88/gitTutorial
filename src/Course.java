7df79193742cff6651af5c36f294b34176bcafb4
cricha88 <cricha88@uwo.ca>
2017-02-15 14:59:47 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Christine on 2/15/2017.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description) {
+        this.cname = cname;
+        this.description = description;
+    }
+}
